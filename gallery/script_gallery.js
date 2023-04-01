1a931354265d311a604b06ef3dea915a3013bf89
Mark_Iachnyi <markyachny@gmail.com>
2023-04-01 18:45:55 +0300

    added dynamic image resizing

--- gallery/script_gallery.js
+++ gallery/script_gallery.js
@@ -3,6 +3,7 @@ Overlay = document.querySelector(".zoom__overlay");
 Close_Button = document.querySelector(".zoom__overlay__close__btn");
 
 Gallery_Images.forEach((element) => {
+    element.style.height = window.innerHeight * 0.3 + 'px';
     element.addEventListener("click",() => {
         document.querySelector(".zoomed__image").src = element.src;
         Overlay.style.display = 'flex';
@@ -12,3 +13,9 @@ Gallery_Images.forEach((element) => {
 Close_Button.addEventListener("click", () => {
     Overlay.style.display = 'none';
 })
+
+window.addEventListener('resize', function(){
+    Gallery_Images.forEach((element) => {
+        element.style.height = window.innerHeight * 0.3 + 'px';
+    })
+}, true);
